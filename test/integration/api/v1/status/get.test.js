afa0cb85ad8363326a6a4d31430d907657b30dee
Joao-Pedrp-J <matheusitalo125@gmail.com>
2025-11-20 16:12:01 -0300

    Adicionado endpoint `/api/v1/status`

---
+++ test/integration/api/v1/status/get.test.js
@@ -0,0 +1,7 @@
+test("GET to api/v1/status should return 200", async () => {
+  const url = "http://localhost:3000/api/v1/status";
+
+  const response = await fetch(url);
+
+  expect(response.status).toBe(200);
+});
